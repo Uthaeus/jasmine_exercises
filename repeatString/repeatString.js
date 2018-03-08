b0aa24fae237ebb0dee9e677a8e4fad2d2fb2fcc
Uthaeus <romanlavery@gmail.com>
2018-03-08 16:38:20 -0700

    repeatString exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-var repeatString = function() {
-
+var repeatString = function(str, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else if (num == 0) {
+    return "";
+  } else {
+    return str.repeat(num);
+  }
 }
 
-module.exports = repeatString
+module.exports = repeatString;
