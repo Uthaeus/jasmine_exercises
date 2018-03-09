c4883d7efa2d6825855f8d0912a106b7e0b9d603
Uthaeus <romanlavery@gmail.com>
2018-03-08 19:11:51 -0700

    Reverse string

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,6 @@
-var reverseString = function() {
-
+var reverseString = function(str) {
+  var revString = str.split('').reverse().join('');
+  return revString;
 }
 
 module.exports = reverseString
