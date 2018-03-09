277bc4f87c61b53f951deaeb811db7a1199a507f
Uthaeus <romanlavery@gmail.com>
2018-03-09 16:36:08 -0700

    Leap year exercise

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,13 @@
-var caesar = function() {
-
+var caesar = function(str, num) {
+  var alph = 'abcdefghijklmnop'.split('');
+  var arr = str.toLower().split('');
+  var result = ''
+  for(var i = 0; i < arr.length; i++) {
+    var strIndex = alph.indexOf(arr[i]);
+    var ind = alph.charAt(strIndex + num);
+    result.push(ind);
+  }
+  return result;
 }
 
 module.exports = caesar
