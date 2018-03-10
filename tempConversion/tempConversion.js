84eb51a1e01d48a64cba55be6dd5cff0b8f2ea1d
Uthaeus <romanlavery@gmail.com>
2018-03-09 21:41:05 -0700

    tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-var ftoc = function() {
-  
+var ftoc = function(temp) {
+  var cel = (temp - 32) / 1.8;
+  return +cel.toFixed(1);
 }
 
-var ctof = function() {
-  
+var ctof = function(temp) {
+  var far = (temp * 1.8) + 32;
+  return +far.toFixed(1);
 }
 
 module.exports = {
